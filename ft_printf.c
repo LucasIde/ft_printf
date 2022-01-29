910be0edc5353900265027051ea2983cb3f7b8d5
Lucas Ide <lide@fu-r9-p5.s19.be>
2022-01-29 08:09:53 +0100

    push rapide

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,36 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 02:23:57 by lide              #+#    #+#             */
-/*   Updated: 2022/01/29 06:09:55 by lide             ###   ########.fr       */
+/*   Updated: 2022/01/29 08:05:39 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	n2;
+	char	n3;
+
+	n2 = n;
+	if (n2 < 0)
+	{
+		n2 *= -1;
+		write(fd, "-", 1);
+	}
+	if (n2 > 9)
+	{
+		ft_putnbr_fd((n2 / 10), fd);
+		n3 = (n2 % 10) + '0';
+		write(fd, &n3, 1);
+	}
+	else
+	{
+		n3 = (n2 % 10) + '0';
+		write(fd, &n3, 1);
+	}
+}
+
 
 int ft_printf(const char *attributes, ...)
 {
@@ -18,10 +43,26 @@ int ft_printf(const char *attributes, ...)
 	int		i;
 
 	i = 0;
-		va_start(arg_ptr, attributes);
+	va_start(arg_ptr, attributes);
 	while (attributes[i])
 	{
-		//recuperer % cspdixX% et y placer va arg dans le type correspondant.
-		va_arg(arg_ptr, attributes);
+		if (attributes[i] == '%')
+		{
+			i++;
+			if (attributes[i] == 'd' || attributes[i] == 'i')
+				ft_putnbr_fd(va_arg(arg_ptr, int), 1);
+			//if (attributes[i] == 'c')
+		}
+		i++;
 	}
+	va_end(arg_ptr);
+	return (0);
+		//va_arg(arg_ptr, attributes);
+		//recuperer % cspdixX% et y placer va arg dans le type correspondant.
+}
+
+int main(void)
+{
+	ft_printf("%d", 150);
+	return (0);
 }
